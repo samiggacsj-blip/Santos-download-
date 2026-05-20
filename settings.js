9b6475fdc41852f079c31b83d42b293f7a5563b4
samiggacsj-blip <samiggac.sj@gmail.com>
2026-05-20 17:57:31 +0200

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'بط اليوتيوبر',
+  author: '‎',
+  botName: ".⤹بــــــــــط الاسطوࢪي'ي               𓅆⤾.",
+  botOwner: 'بــــــــــط الاسطوࢪي'ي               𓅆', // Your name
+  ownerNumber: '+249967109953'    ', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.4",
+  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
